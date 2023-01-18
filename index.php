f88210a4afb3988dfb98ef8df4ddd8797a7415a9
Marie Cari <mcari@nyc.yearup.org>
2023-01-18 12:35:51 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seeds of Life Home Page</title>
+    <link rel="stylesheet" href="./css/style.css"> 
+    <!-- icons  -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        <div class="container nav__container">
+            <a href="index.html" class="nav__logo">Seeds of Life</a>
+            <ul class="nav__items">
+                <li><a href="blog.php">Foods In Season</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <!-- <li><a href="signin.html">SignIn</a></li> -->
+                <li class="nav__profile">
+                    <div class="avatar">
+                        <img src="./images/farmer1_avatar.png">
+                    </div>
+                    <ul>
+                        <li><a href="dashboard.html">Dashboard</a></li>
+                        <li><a href="logout.html">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+            
+            <button id="open__nav-btn"><i class="uil uil-bars"></i></button>
+            <button id="close__nav-btn"><i class="uil uil-multiply"></i></button>
+         </div>
+    </nav>
+    <!-- end of nav -->
+
+    <section class="featured"> 
+        <div class="container featured__container">
+            <div class="post__thumbnail">
+                <img src="./images/farmer1_avatar.png">
+            </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Foods In Season</a>
+                    <h2 class="post__title"><a href="post.html"> Lorem ipsum, doloar, sit amet </a></h2>
+                    <p class="post_body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ratione inventore adipisci nobis nisi iste sit eligendi quos sapiente, delectus accusantium excepturi ex nemo labore necessitatibus maxime suscipit earum aperiam!</p>
+                </div>
+                <div class="post__author">
+                    <div class="post__author-avatar">
+                        <img src="./images/farmer1_avatar.png">
+                    </div>
+                    <div class="post__author-info">
+                        <h5>By: Jane Doe</h5>
+                        <small>January 11, 2022 - 07:23</small>
+                    </div>
+                </div>
+        </div>
+    </section>
+    <!-- end of featured -->
+
+    <section class="posts">
+        <div class="container posts__container">
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="./images/oranges.jpg">
+                </div>
+                <div class="post__info">
+                    <a href="category-posts.html" class="category__button">Foods In Seaso</a>
+                    <h3 class="post_title">
+                        <a href="post.html"> Oranges Now In Season </a>
+                    </h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur mollitia, corrupti consequatur illum iusto nemo tempora.
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/farmer1_avatar.png">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Doe </h5>
+                            <small>
+                                July 21, 2005 - 10:30
+                            </small>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="./images/chopping-vegetables-1.webp">
+                </div>
+                <div class="post__info">
+                    <a href="category-posts.html" class="category__button">Recipes</a>
+                    <h3 class="post_title">
+                        <a href="post.html">What To Make With Your Fresh Produce </a>
+                    </h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur mollitia, corrupti consequatur illum iusto nemo tempora.
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/avatar2.png">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Doe </h5>
+                            <small>
+                                July 21, 2005 - 10:30
+                            </small>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="./images/farmer1.jpg">
+                </div>
+                <div class="post__info">
+                    <a href="category-posts.html" class="category__button">Farmer Bios</a>
+                    <h3 class="post_title">
+                        <a href="post.html"> Meet Local Farmer: Bob </a>
+                    </h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur mollitia, corrupti consequatur illum iusto nemo tempora.
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/avatar3.jpg">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Doe </h5>
+                            <small>
+                                July 21, 2005 - 10:30
+                            </small>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="./images/pomegranate.jpg">
+                </div>
+                <div class="post__info">
+                    <a href="category-posts.html" class="category__button">Foods In Season</a>
+                    <h3 class="post_title">
+                        <a href="post.html">Winter Times Means Pomegranate Season </a>
+                    </h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur mollitia, corrupti consequatur illum iusto nemo tempora.
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/farmer1_avatar.png">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Doe </h5>
+                            <small>
+                                July 21, 2005 - 10:30
+                            </small>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="./images/apples.jpg">
+                </div>
+                <div class="post__info">
+                    <a href="category-posts.html" class="category__button">Foods In Season</a>
+                    <h3 class="post_title">
+                        <a href="post.html"> Apples Are Now In Season: Get Them Fresh </a>
+                    </h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur mollitia, corrupti consequatur illum iusto nemo tempora.
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/avatar4.jpg">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Doe </h5>
+                            <small>
+                                July 21, 2005 - 10:30
+                            </small>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="./images/farmer1_avatar.png">
+                </div>
+                <div class="post__info">
+                    <a href="category-posts.html" class="category__button">Category</a>
+                    <h3 class="post_title">
+                        <a href="post.html">Lorem ipsum dolor sit. Lorem ipsum dolor sit. </a>
+                    </h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur mollitia, corrupti consequatur illum iusto nemo tempora.
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/farmer1_avatar.png">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: John Doe </h5>
+                            <small>
+                                July 21, 2005 - 10:30
+                            </small>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </section>
+
+    <!-- start of categrory -->
+    <section class="category__buttons">
+        <div class="container category__buttons-container">
+            <a href="" class="category__button">Foods in Season</a>
+            <a href="" class="category__button">Farmer Bios</a>
+            <a href="" class="category__button">Recipes</a>
+            <a href="" class="category__button">Money Saving Resources</a>
+
+        </div>
+    </section>
+
+    <!-- start of footer -->
+    <footer>
+        <div class="footer__socials">
+            <a href="https://youtube.com" target="_blank"><i class="uil uil-youtube"></i></a>
+            <a href="https://facebook.com" target="_blank"><i class="uil uil-facebook-f"></i></a>
+            <a href="https://instagram.com" target="_blank"><i class="uil uil-instagram-alt"></i></a>
+            <a href="https://linkedin.com" target="_blank"><i class="uil uil-linkedin"></i></a>
+            <a href="https://twitter.com" target="_blank"><i class="uil uil-twitter"></i></a>
+        </div>
+
+        <div class="container footer__container">
+            <article>
+                <h4>Categories</h4>
+                <ul>
+                    <li><a href="">Foods In Season</a></li>
+                    <li><a href="">Farmer Bios</a></li>
+                    <li><a href="">Recipes</a></li>
+                    <li><a href="">Money Saving Resources</a></li>
+                </ul>
+            </article>
+            <article>
+                <h4>Support</h4>
+                <ul>
+                    <li><a href="">Online Support</a></li>
+                    <li><a href="">Call Numbers</a></li>
+                    <li><a href="">Email</a></li>
+                    <li><a href="">Social Support</a></li>
+                    <li><a href="">Locations</a></li>
+                </ul>
+            </article>
+            <article>
+                <h4>Blog</h4>
+                <ul>
+                    <li><a href="">Random 1</a></li>
+                    <li><a href="">Rnadom 2</a></li>
+                    <li><a href="">Random 3</a></li>
+                    <li><a href="">Random 4</a></li>
+                    <li><a href="">Random 5</a></li>
+                </ul>
+            </article>
+            <article>
+                <h4>Permalinks</h4>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Blog</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Services</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </article>
+        </div>
+        <div class="footer__copyright">
+            <small>Copyright &copy; MARIE </small>
+        </div>
+    </footer>
+
+    <script src="./main.js"></script>
+</body>
+</html>

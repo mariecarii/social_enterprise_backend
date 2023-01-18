ba3f50c193c7e020f48de2a8e504e8709c625141
Marie Cari <mcari@nyc.yearup.org>
2023-01-18 12:47:00 -0500

    aded footer tp partials footer file so it can be applied without repeating code

--- index.php
+++ index.php
@@ -192,62 +192,7 @@ include 'partials/header.php'
         </div>
     </section>
 
-    <!-- start of footer -->
-    <footer>
-        <div class="footer__socials">
-            <a href="https://youtube.com" target="_blank"><i class="uil uil-youtube"></i></a>
-            <a href="https://facebook.com" target="_blank"><i class="uil uil-facebook-f"></i></a>
-            <a href="https://instagram.com" target="_blank"><i class="uil uil-instagram-alt"></i></a>
-            <a href="https://linkedin.com" target="_blank"><i class="uil uil-linkedin"></i></a>
-            <a href="https://twitter.com" target="_blank"><i class="uil uil-twitter"></i></a>
-        </div>
-
-        <div class="container footer__container">
-            <article>
-                <h4>Categories</h4>
-                <ul>
-                    <li><a href="">Foods In Season</a></li>
-                    <li><a href="">Farmer Bios</a></li>
-                    <li><a href="">Recipes</a></li>
-                    <li><a href="">Money Saving Resources</a></li>
-                </ul>
-            </article>
-            <article>
-                <h4>Support</h4>
-                <ul>
-                    <li><a href="">Online Support</a></li>
-                    <li><a href="">Call Numbers</a></li>
-                    <li><a href="">Email</a></li>
-                    <li><a href="">Social Support</a></li>
-                    <li><a href="">Locations</a></li>
-                </ul>
-            </article>
-            <article>
-                <h4>Blog</h4>
-                <ul>
-                    <li><a href="">Random 1</a></li>
-                    <li><a href="">Rnadom 2</a></li>
-                    <li><a href="">Random 3</a></li>
-                    <li><a href="">Random 4</a></li>
-                    <li><a href="">Random 5</a></li>
-                </ul>
-            </article>
-            <article>
-                <h4>Permalinks</h4>
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Blog</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Services</a></li>
-                    <li><a href="">Contact</a></li>
-                </ul>
-            </article>
-        </div>
-        <div class="footer__copyright">
-            <small>Copyright &copy; MARIE </small>
-        </div>
-    </footer>
-
-    <script src="./main.js"></script>
-</body>
-</html>
+   
+<?php
+include 'partials/footer.php'
+?>
